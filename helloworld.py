639694e6e479956bb438cba6d9aaff5b4b4a23e4
Katie <159644127+katums@users.noreply.github.com>
2026-03-13 16:04:31 -0600

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print('Hello World!')
+name = 'Katie'
+print('My name is ', name)
+print('I am learning Python')
+print(name, type(name))
+print(isinstance(name, int))
+is_learning_python = True
+print(is_learning_python, type(is_learning_python))
