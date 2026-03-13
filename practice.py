f121cc6d6c07efff93308908879cda9fbb67509c
Katie <159644127+katums@users.noreply.github.com>
2026-03-13 15:45:09 -0600

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,1 @@
-print('Hello world!')
-name = 'Katie'
-print('My name is:', name)
-print(name)
-print(type(name))
-
-is_learning_python = True
-print(is_learning_python, type(is_learning_python))
-print(isinstance(name, int))
 
