dab28e1f0a16d9035eb931f3de350cd41f1c9c1a
Katie <159644127+katums@users.noreply.github.com>
2026-03-19 16:15:09 -0600

    Update stringpractice.py

--- stringpractice.py
+++ stringpractice.py
@@ -1,8 +1,5 @@
 name = "Bob"
 age = 42
 
-name_plus_age = name # Start with the name
-name_plus_age += str(age) # Change the age to a string
-
+name_plus_age = f"His name is {name} and he's {age} years old."
 print(name_plus_age)
-
